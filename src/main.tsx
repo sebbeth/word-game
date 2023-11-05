d522ee85e83afc0f12f432b8266eae21360d8d99
Seb Brown <sbrown@camplify.com.au>
2023-11-05 22:26:20 +1100

    pixi setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BlurBoi } from "./BlurBoi.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BlurBoi />
+  </React.StrictMode>
+);
