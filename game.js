1f24f9f40d71a80f2aa372665585954447c081b8
Seb Brown <sbrown@camplify.com.au>
2023-11-07 23:09:06 +1100

    Add UI buttons and player movement

--- game.js
+++ game.js
@@ -1,13 +1,14 @@
 import {
-  GAME_HEIGHT,
-  GAME_WIDTH,
   GROUND_POSITION,
+  PLAYER_JUMP_FORCE,
   applyGravity,
+  applyPlayerMovement,
+  isTouchingGround,
 } from "./physics.js";
 
 const objectsWithMass = [];
 
-function spriteFactory(
+function objectFactory(
   params = {
     x,
     y,
@@ -35,37 +36,108 @@ function spriteFactory(
 }
 
 function createGround() {
-  const obj = new PIXI.Graphics();
-  obj.beginFill(0x008000);
-  obj.drawRect(0, GAME_HEIGHT - GROUND_POSITION, GAME_WIDTH, GROUND_POSITION);
-  app.stage.addChild(obj);
+  const sprite = PIXI.Sprite.from("assets/ground.png");
+  sprite.height = GROUND_POSITION;
+  sprite.width = app.view.width;
+  sprite.y = app.view.height - sprite.height;
+  app.stage.addChild(sprite);
 }
 
-function getBunny() {
-  const bunny = PIXI.Sprite.from("https://pixijs.com/assets/bunny.png");
+function createBackground() {
+  const sprite = PIXI.Sprite.from("assets/paper.png");
+  sprite.height = app.view.height;
+  sprite.width = app.view.width;
+  app.stage.addChild(sprite);
+}
 
-  // center the sprite's anchor point
-  bunny.anchor.set(0.5);
+function createButton(x, y, scale, text, onClick) {
+  // TODO, make this better
+  const basicText = new PIXI.Text(text);
+  const button = PIXI.Sprite.from("assets/button.png");
+  button.x = x;
+  button.y = y;
+  button.scale = scale;
+  button.anchor.set(0.5);
+  button.alpha = 0.7;
+  button.eventMode = "static";
+  button.cursor = "pointer";
+  button.addChild(basicText);
+  button.on("pointerdown", onClick);
+  app.stage.addChild(button);
+}
 
-  // move the sprite to the center of the screen
-  bunny.on("pointerdown", () => {
-    bunny.scale.x *= 1.25;
+function createUI(player) {
+  createButton(100, 100, { x: 0.5, y: 0.5 }, "<", () => {
+    movePlayer({ player, direction: "left" });
   });
-  bunny.x = app.screen.width / 2;
-  bunny.y = app.screen.height / 2;
-  return bunny;
+  createButton(350, 100, { x: 0.5, y: 0.5 }, "^", () => {
+    movePlayer({ player, direction: "jump" });
+  });
+  createButton(600, 100, { x: 0.5, y: 0.5 }, ">", () => {
+    movePlayer({ player, direction: "right" });
+  });
 }
+
+function createTree(x, y) {
+  const sprite = PIXI.Sprite.from("assets/tree.png");
+  sprite.scale.x = 0.2;
+  sprite.scale.y = 0.2;
+  sprite.anchor.x = 0.5;
+  sprite.anchor.y = 1;
+  sprite.x = x;
+  sprite.y = y;
+  app.stage.addChild(sprite);
+}
+
+function movePlayer(params = { player, direction }) {
+  const { direction, player } = params;
+  switch (direction) {
+    case "left":
+      if (isTouchingGround(app, player)) {
+        player.velocity.x = player.velocity.x - 1;
+      }
+      break;
+
+    case "right":
+      console.log(player.velocity);
+      if (isTouchingGround(app, player)) {
+        player.velocity.x += 1;
+      }
+      break;
+
+    case "jump":
+      if (isTouchingGround(app, player)) {
+        player.velocity.y = -PLAYER_JUMP_FORCE;
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+
 // Create the application helper and add its render target to the page
-let app = new PIXI.Application({ width: GAME_WIDTH, height: GAME_HEIGHT });
+let app = new PIXI.Application({ resizeTo: window });
 document.body.appendChild(app.view);
 
+createBackground();
 createGround();
+const player = objectFactory({
+  x: 100,
+  y: app.view.height - GROUND_POSITION - 50,
+  height: 60,
+  width: 40,
+  texture: "assets/lilguy.png",
+  hasMass: true,
+});
+createUI(player);
 
-const box = spriteFactory({ x: 100, y: 100, hasMass: true });
+[30, 150, 204, 300, 412, 434, 484, 600, 221, 650].forEach((x) =>
+  createTree(x, app.view.height - GROUND_POSITION + 20)
+);
 
 // Listen for animate update
 app.ticker.add((delta) => {
-  //   bunny.rotation += 0.1 * delta;
-  // apply gravity
-  applyGravity(delta, objectsWithMass);
+  applyPlayerMovement(app, delta, player);
+  applyGravity(app, delta, objectsWithMass);
 });
--- physics.js
+++ physics.js
@@ -1,23 +1,47 @@
 export const GROUND_POSITION = 50;
-export const GRAVITY = 0.03;
-export const GAME_HEIGHT = 400;
-export const GAME_WIDTH = 800;
+export const GRAVITY = 0.3;
+export const PLAYER_JUMP_FORCE = 4;
+export const GROUND_FRICTION = 0.02;
 
-export function hasHitGround(sprite) {
-  const feetPosition = sprite.y + sprite.height / 2;
-  if (feetPosition > GAME_HEIGHT - GROUND_POSITION) {
+export function isTouchingGround(app, sprite) {
+  const feetPosition = sprite.y + sprite.height / 2 + 5; // add some padding
+
+  if (feetPosition > app.view.height - GROUND_POSITION) {
     return true;
   }
   return false;
 }
 
-export function applyGravity(delta, objectsWithMass) {
+export function applyGravity(app, delta, objectsWithMass) {
   objectsWithMass.forEach((sprite) => {
-    if (hasHitGround(sprite)) {
-      sprite.velocity = 0;
+    if (isTouchingGround(app, sprite) && sprite.velocity.y > 0) {
+      sprite.velocity.y = 0;
+      sprite.y = app.view.height - GROUND_POSITION - sprite.height / 2;
       return;
     }
     sprite.velocity.y = sprite.velocity.y + GRAVITY * delta;
     sprite.y += sprite.velocity.y * delta;
   });
+}
+
+export function applyPlayerMovement(app, delta, player) {
+  const leftWall = 0;
+  const rightWall = app.view.width;
+
+  if (player.x - player.width / 2 < leftWall) {
+    player.x = leftWall + player.width / 2;
+    player.velocity.x = 0;
+  }
+  if (player.x + player.width / 2 > rightWall) {
+    player.x = rightWall - player.width / 2;
+    player.velocity.x = 0;
+  }
+
+  if (Math.abs(player.velocity.x) > 0) {
+    player.velocity.x =
+      player.velocity.x > 0
+        ? player.velocity.x - GROUND_FRICTION
+        : player.velocity.x + GROUND_FRICTION;
+  }
+  player.x += player.velocity.x * delta;
 }
